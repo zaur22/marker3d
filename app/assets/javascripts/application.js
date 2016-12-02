0c62c170731013f15dc43c1100febd17d1f52f23
Zaur22 <mr.zaur22@gmail.com>
2016-12-02 14:03:34 +0300

    release

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,57 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery.slick	
+//= require_tree .
+
+
+$(function(){
+	$('body').scrollspy({ target: '#navbar-example' });
+
+	$('.slick-carousel').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+    	autoplaySpeed: 0,
+    	speed: 20000,
+    	//variableWidth: true,
+    	infinite: true,
+    	cssEase: 'linear',
+    	easing: 'linear',
+    	//adaptiveHeight: true
+  	});
+
+	$('.slick-3dpen-carousel').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: false,
+		pauseOnHover: false,
+		autoplay: true,
+    	autoplaySpeed: 0,
+    	speed: 20000,
+    	variableWidth: true,
+    	infinite: true,
+    	cssEase: 'linear',
+    	easing: 'linear',
+    	adaptiveHeight: true
+  	});
+  	$( window ).scroll(function(){
+		if($("#navbar").offset().top > 0){
+			$("#navbar").addClass("white-nav");
+		}else{
+			$("#navbar").removeClass("white-nav");
+		}
+  	})
+});
