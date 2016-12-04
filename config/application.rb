d934d66d84ed0ea8318d9f4b6d93b76b71229214
Zaur22 <mr.zaur22@gmail.com>
2016-12-04 13:32:48 +0300

    "hi"

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.secret_key_base = 'abrbrbrbabr'
+
 
 module Marker3D
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
